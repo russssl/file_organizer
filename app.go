7fb49832104b3066fe6ffca7cee5772e9403fcb9
Ruslan Uriadko <70024944+russssl@users.noreply.github.com>
2024-08-30 22:45:20 +0200

    add better error cases handling, move default config into main file

--- app.go
+++ app.go
@@ -13,8 +13,32 @@ type Config struct {
 	Rules map[string]string `json:"rules"`
 }
 
+var defaultConfig = Config{
+	Rules: map[string]string{
+		".png":  "images",
+		".jpg":  "images",
+		".jpeg": "images",
+		".gif":  "images",
+		".txt":  "documents",
+		".pdf":  "documents",
+		".doc":  "documents",
+		".docx": "documents",
+		".xlsx": "spreadsheets",
+		".xls":  "spreadsheets",
+		".csv":  "spreadsheets",
+		".mp3":  "audio",
+		".wav":  "audio",
+		".mp4":  "videos",
+		".mkv":  "videos",
+		".avi":  "videos",
+	},
+}
+
 func loadConfig(configPath string) (Config, error) {
 	var config Config
+	if configPath == "" {
+		return defaultConfig, nil
+	}
 	file, err := os.Open(configPath)
 	if err != nil {
 		return config, err
@@ -29,16 +53,16 @@ func loadConfig(configPath string) (Config, error) {
 	return config, nil
 }
 
-func organizeFiles(directory string, config Config) error {
-	return filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+func organizeFiles(directory string, config Config, recursive bool) error {
+	filesMoved := 0
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if info.IsDir() {
+		if info.IsDir() || (!recursive && filepath.Dir(path) != directory) {
 			return nil
 		}
-
 		ext := filepath.Ext(info.Name())
 		if destDir, ok := config.Rules[ext]; ok {
 			destPath := filepath.Join(directory, destDir, info.Name())
@@ -48,28 +72,47 @@ func organizeFiles(directory string, config Config) error {
 				return err
 			}
 			fmt.Printf("Moved %s to %s\n", path, destPath)
+			filesMoved++
 		}
-
 		return nil
 	})
+	if filesMoved == 0 {
+		fmt.Println("No files were moved")
+	}
+	return err
 }
 
 func main() {
-	configPath := flag.String("config", "example_config.json", "Path to the configuration file")
+	configPath := flag.String("config", "", "Path to the configuration file")
 	directory := flag.String("dir", ".", "Directory to organize")
 	help := flag.Bool("help", false, "Show help message")
+	printDefault := flag.Bool("print-default", false, "Print default config")
+	recursive := flag.Bool("r", false, "Organize files recursively, including subdirectories")
+	v := flag.Bool("v", false, "Show version")
+
 	flag.Parse()
 
 	if *help {
 		flag.PrintDefaults()
 		return
 	}
+	if *v {
+		fmt.Println("v0.0.1")
+		return
+	}
+
 	config, err := loadConfig(*configPath)
 	if err != nil {
 		log.Fatalf("Error loading config: %v\n", err)
 	}
 
-	err = organizeFiles(*directory, config)
+	if *printDefault {
+		defaultConfigJson, _ := json.MarshalIndent(defaultConfig, "", "  ")
+		fmt.Println(string(defaultConfigJson))
+		return
+	}
+	err = organizeFiles(*directory, config, *recursive)
+
 	if err != nil {
 		log.Fatalf("Error organizing files: %v\n", err)
 	}
